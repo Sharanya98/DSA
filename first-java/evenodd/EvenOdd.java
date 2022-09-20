1e051e08167bb38232acb2dfafc748199e028ff2
sbhongade598@gmail.com <sbhongade598@gmail.com>
2022-09-20 21:47:23 +0900

    evenodd

--- first-java/evenodd/EvenOdd.java
+++ first-java/evenodd/EvenOdd.java
@@ -1,7 +1,18 @@
 package DSA.firstjava.E1;
 
+import java.util.Scanner;
+
 public class EvenOdd {
     public static void main(String[] args) {
-        System.out.println("working");
+        System.out.println("Enter a number");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        boolean result = num%2 ==0? false: true;
+        if(result){
+            System.out.println("Number is odd");
+        }
+        else{
+            System.out.println("Number is even");
+        }
     }
 }
